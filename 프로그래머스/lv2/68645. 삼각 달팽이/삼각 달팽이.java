4d8530068d212074b98e8f544aa530302a8375d6
Changha, Choi <alklid@gmail.com>
2023-08-02 21:39:17 +0900

    [level 2] Title: 삼각 달팽이, Time: 23.27 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,4 +1,8 @@
 class Solution {
+    // 좌표의 방향 이동 : 아래, 오른쪽, 왼쪽 위
+    private static final int[] dx = {0, 1, -1};
+    private static final int[] dy = {1, 0, -1};
+    
     public int[] solution(int n) {
         // 길이, 높이가 n인 이차원 배열 생성
         int[][] triangle = new int[n][n];
@@ -10,58 +14,39 @@ class Solution {
         int x = 0;
         int y = 0;
 
+        // 방향
+        int d = 0;
+
         while (true) {
+            triangle[y][x] = value++;
+
             // 아래로 이동
-            while (true) {
-                triangle[y][x] = value++;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
 
-                // 아래로 갈 수 없으면(마지막 줄이거나, 아래칸의 값이 이미 채워져 있으면) 종료
-                if (y + 1 == n || triangle[y + 1][x] != 0) break;
-                y++;
+            // 더 이상 진행 불가시 방향 전환
+            if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
+                d = (d + 1) % 3;
+                nx = x + dx[d];
+                ny = y + dy[d];
+
+                // 방향 전환을 했는데도 진행 불가시 종료
+                if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
+                    break;
+                }
             }
 
-            // 오른쪽으로 갈 수 없으면(가장 오른쪽이거나, 오른쪽의 값이 이미 채워져 있으면) 종료
-            if (x + 1 == n || triangle[y][x+1] != 0) break;
-            x++;
-
-            // 오른쪽으로 이동
-            while (true) {
-                triangle[y][x] = value++;
-
-                // 오른쪽으로 갈 수 없으면(가장 오른쪽이거나, 오른쪽의 값이 이미 채워져 있으면) 종료
-                if (x + 1 == n || triangle[y][x+1] != 0) break;
-                x++;
-            }
-
-            // 위쪽으로 갈 수 없으면(위칸의 값이 이미 채워져 있으면) 종료
-            if (triangle[y-1][x-1] != 0) break;
-            x--;
-            y--;
-
-            // 위로 이동
-            while (true) {
-                triangle[y][x] = value++;
-
-                // 위쪽으로 갈 수 없으면(위칸의 값이 이미 채워져 있으면) 종료
-                if (triangle[y-1][x-1] != 0) break;
-                x--;
-                y--;
-            }
-
-            // 아래로 갈 수 없으면(마지막 줄이거나, 아래칸의 값이 이미 채워져 있으면) 종료
-            if (y + 1 == n || triangle[y + 1][x] != 0) break;
-            y++;
+            // 전환된 방향으로 이동
+            x = nx;
+            y = ny;
         }
-
 
         // 채워진 배열에서 0을 제외하고 순서대로 합치기
         int[] answer = new int[value-1];
         int index = 0;
-        for (int j=0; j<n; j++) {
-            for (int i=0; i<n; i++) {
-                if (triangle[j][i] > 0) {
-                    answer[index++] = triangle[j][i];
-                }
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<=i; j++) {
+                answer[index++] = triangle[i][j];
             }
         }
 
