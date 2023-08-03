ca7e7ab15155940e00b4765a61ebfa07e98204a6
Changha, Choi <alklid@gmail.com>
2023-08-03 12:16:50 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.09 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        validation(my_string, is_prefix);
+        
+        int answer = (my_string.startsWith(is_prefix)) ? 1 : 0;
+        return answer;
+    }
+    
+    static void validation(String my_string, String is_prefix) {
+        int strLen = my_string.length();
+        if (strLen < 1 || strLen > 100) {
+            // bad input, throw exception
+        }
+        
+        int prefixLen = is_prefix.length();
+        if (prefixLen < 1 || prefixLen > 100) {
+            // bad input, throw exception
+        }
+        
+        if (!my_string.matches("^[a-z]$")) {
+            // bad input, throw exception
+        }
+        
+        if (!is_prefix.matches("^[a-z]$")) {
+            // bad input, throw exception
+        }
+    }
+}
