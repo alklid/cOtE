bdd64702395c79cdff6fafc30f67de9d84c761a4
Changha, Choi <alklid@gmail.com>
2023-08-03 11:50:13 +0900

    [unrated] Title: 문자열의 뒤의 n글자, Time: 0.11 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        validation(my_string, n);
+        
+        int len = my_string.length();
+        String answer = my_string.substring(len-n);
+        return answer;
+    }
+    
+    static void validation(String my_string, int n) {
+        int len = my_string.length();
+        if (len < 1 || len > 1000) {
+            // bad input, throw exception
+        }
+        
+        if (n < 1 || n > len) {
+            // bad input, throw exception
+        }
+        
+        if (!my_string.matches("^[a-zA-Z0-9]$")) {
+            // bad input, throw exception
+        }
+    }
+}
