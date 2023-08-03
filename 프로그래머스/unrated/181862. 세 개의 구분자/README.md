ddf052e62c7c8d00e0cb07eec6dfcbbfb6845c8f
Changha, Choi <alklid@gmail.com>
2023-08-03 20:32:59 +0900

    [unrated] Title: 세 개의 구분자, Time: 43.05 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/README.md
+++ 프로그래머스/unrated/181862. 세 개의 구분자/README.md
@@ -1,10 +1,10 @@
 # [unrated] 세 개의 구분자 - 181862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181862?language=java) 
 
 ### 성능 요약
 
-메모리: 102 MB, 시간: 46.22 ms
+메모리: 108 MB, 시간: 43.05 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
@@ -3,7 +3,7 @@ import java.util.stream.Stream;
 class Solution {
     public String[] solution(String myStr) {
         String[] answer = Stream.of(myStr.split("[abc]"))
-                            .filter(str -> str.length() > 0)
+                            .filter(str -> !str.isEmpty())
                             .toArray(String[]::new);
         if (answer.length == 0) {
             answer = new String[] {"EMPTY"};
