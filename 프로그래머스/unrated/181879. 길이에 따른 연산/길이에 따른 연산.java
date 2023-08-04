c11c9a36e86669f482d6564b7042a5a22682cc77
Changha, Choi <alklid@gmail.com>
2023-08-04 14:42:25 +0900

    [unrated] Title: 길이에 따른 연산, Time: 0.02 ms, Memory: 69 MB -BaekjoonHub

--- 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -4,6 +4,7 @@ class Solution {
     public int solution(int[] num_list) {
         int answer = (num_list.length >= 11) ? 0 : 1;
         
+        /*
         if (num_list.length >= 11) {
             // 모든 원소의 합
             answer = Arrays.stream(num_list).sum();
@@ -11,6 +12,18 @@ class Solution {
         else {
             // 모든 원소의 곱
             answer = Arrays.stream(num_list).reduce(1, (a, b) -> a * b);
+        }
+        */
+        
+        for (int i=0; i<num_list.length; i++) {
+            if (num_list.length >= 11) {
+                // 모든 원소의 합
+                answer += num_list[i];
+            }
+            else {
+                // 모든 원소의 곱
+                answer *= num_list[i];
+            }
         }
         
         return answer;
