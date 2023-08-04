40a381459c2952fcb9aa36e0d09b9d4190b907bd
Changha, Choi <alklid@gmail.com>
2023-08-04 15:38:55 +0900

    [unrated] Title: n개 간격의 원소들, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int len = (num_list.length % n == 0) ? num_list.length / n : (num_list.length / n) + 1;
+        
+        int[] answer = new int[len];
+        for (int i=0; i<num_list.length; i=i+n) {
+            answer[i/n] = num_list[i];
+        }
+        
+        return answer;
+    }
+}
