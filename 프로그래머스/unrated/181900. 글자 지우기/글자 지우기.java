ea439752fba4c4bb48df101d28f77c3a1c2a8e62
Changha, Choi <alklid@gmail.com>
2023-08-05 20:12:54 +0900

    [unrated] Title: 글자 지우기, Time: 0.39 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+import java.lang.String;
+
 class Solution {
     public String solution(String my_string, int[] indices) {
+        
+        String[] filter = my_string.split("");
+        for (int idx : indices) {
+            filter[idx] = "";
+        }
+        
+        String answer = String.join("", filter);
+        return answer;
+        
+        /*
         String answer = my_string;
         for (int idx : indices) {
             int prefixLastIdx = idx;
@@ -11,5 +24,6 @@ class Solution {
         }
         
         return answer.replace("_", "");
+        */
     }
 }
