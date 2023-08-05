97b6a347d2901f151058f96ded6cd54af5714045
Changha, Choi <alklid@gmail.com>
2023-08-05 17:13:21 +0900

    [unrated] Title: 할 일 목록, Time: 0.04 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -4,7 +4,8 @@ import java.util.List;
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
         int len = todo_list.length;
-            
+        
+        /* 엄청 느림
         String todo = "";
         for (int i=0; i<len; i++) {
             if (finished[i] == false) {
@@ -13,6 +14,16 @@ class Solution {
         }
         
         String[] answer = todo.split(",");
+        */
+        
+        List<String> todo = new ArrayList<String>();
+        for (int i=0; i<len; i++) {
+            if (finished[i] == false) {
+                todo.add(todo_list[i]);
+            }
+        }
+
+        String[] answer = todo.toArray(new String[todo.size()]);
         return answer;
     }
 }
