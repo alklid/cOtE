975673beb48d7514dde475c58859438e0b220a44
Changha, Choi <alklid@gmail.com>
2023-08-05 22:17:38 +0900

    [unrated] Title: 리스트 자르기, Time: 6.10 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        int startIdx = 0;
+        int endIdx = 0;
+        int interval = 1;
+        
+        if (n == 1) {
+            startIdx = 0;
+            endIdx = slicer[1];
+            interval = 1;
+        }
+        if (n == 2) {
+            startIdx = slicer[0];
+            endIdx = num_list.length-1;
+            interval = 1;
+        }
+        if (n == 3) {
+            startIdx = slicer[0];
+            endIdx = slicer[1];
+            interval = 1;
+        }
+        if (n == 4) {
+            startIdx = slicer[0];
+            endIdx = slicer[1];
+            interval = 2;
+        }
+        
+        int len = ((endIdx - startIdx + 1) % interval == 0) ? 
+                        (endIdx - startIdx + 1) / interval : 
+                        ((endIdx - startIdx + 1) / interval) + 1;
+        int[] answer = new int[len];
+        
+        int idx = 0;
+        for (int i=startIdx; i<=endIdx; i+=interval) {
+            System.out.println("i : " + i);
+            answer[idx] = num_list[i];
+            idx++;
+        }
+        return answer;
+    }
+    
+}
