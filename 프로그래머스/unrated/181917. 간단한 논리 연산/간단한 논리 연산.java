91705a28b7cf7685875be97a552266cd990e365a
Changha, Choi <alklid@gmail.com>
2023-08-06 19:53:51 +0900

    [unrated] Title: 간단한 논리 연산, Time: 0.02 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.java
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -1,11 +1,6 @@
 class Solution {
     public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-        int i1 = x1 ? 1 : 0;
-        int i2 = x2 ? 1 : 0;
-        int i3 = x3 ? 1 : 0;
-        int i4 = x4 ? 1 : 0;
-         
-        boolean answer = (((i1 | i2) & (i3 | i4)) == 1) ? true : false;
+        boolean answer = (x1 || x2) && (x3 || x4);
         return answer;
     }
 }
