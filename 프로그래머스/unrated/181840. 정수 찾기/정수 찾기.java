a99c6ec173a14df46ce0e294f7b96bf317239238
Changha, Choi <alklid@gmail.com>
2023-08-06 22:55:05 +0900

    [unrated] Title: 정수 찾기, Time: 0.02 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -2,11 +2,23 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] num_list, int n) {
+        /*
         int answer = Arrays.stream(num_list)
                         .filter(num -> num == n)
                         .map(num -> 1)
                         .findFirst()
                         .orElse(0);
+        return answer;
+        */
+        
+        int answer = 0;
+        for (int num : num_list) {
+            if (num == n) {
+                answer = 1;
+                break;
+            }
+        }
+        
         return answer;
     }
 }
