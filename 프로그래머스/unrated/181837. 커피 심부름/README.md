758741c95b3307eb62a15fb58016c599cd0d8f18
Changha, Choi <alklid@gmail.com>
2023-08-06 23:35:22 +0900

    [unrated] Title: 커피 심부름, Time: 0.03 ms, Memory: 68.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/README.md
+++ 프로그래머스/unrated/181837. 커피 심부름/README.md
@@ -1,10 +1,10 @@
 # [unrated] 커피 심부름 - 181837 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181837) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181837?language=java) 
 
 ### 성능 요약
 
-메모리: 75.4 MB, 시간: 0.04 ms
+메모리: 68.4 MB, 시간: 0.03 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
@@ -1,15 +1,11 @@
 class Solution {
     public int solution(String[] order) {
         int answer = 0;
-        for (String item : order) {
-            String coffee = item.replace("ice", "")
-                                .replace("hot", "")
-                                .replace("anything", "americano");
-            if (coffee.equals("americano")) {
-                answer += 4500;
-            }
-            if (coffee.equals("cafelatte")) {
+        for (String coffee : order) {
+            if (coffee.contains("cafelatte")) {
                 answer += 5000;
+            } else {
+                answer += 4500;
             }
         }
         
