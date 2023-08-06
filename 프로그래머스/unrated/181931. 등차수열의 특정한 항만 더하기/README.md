62ca3564ff4fcf0b77ad1246191100304ff3feae
Changha, Choi <alklid@gmail.com>
2023-08-06 17:06:24 +0900

    [unrated] Title: 등차수열의 특정한 항만 더하기, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/README.md
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 등차수열의 특정한 항만 더하기 - 181931 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181931) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181931?language=java) 
 
 ### 성능 요약
 
-메모리: 78.2 MB, 시간: 0.04 ms
+메모리: 74.1 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -1,14 +1,16 @@
 class Solution {
     public int solution(int a, int d, boolean[] included) {
+        /*
         int[] arr = new int[included.length];
         for (int i=0; i<arr.length; i++) {
             arr[i] = a + (i * d);
         }
+        */
         
         int answer = 0;
         for (int i=0; i<included.length; i++) {
             if (included[i]) {
-                answer += arr[i];
+                answer += a + (i * d);
             }
         }
         
