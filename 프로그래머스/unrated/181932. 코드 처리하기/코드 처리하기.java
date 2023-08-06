3502972ed4ee3a49be8f267a6812532088458ed0
Changha, Choi <alklid@gmail.com>
2023-08-06 16:54:08 +0900

    [unrated] Title: 코드 처리하기, Time: 0.08 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String solution(String code) {
+        StringBuffer ret = new StringBuffer();
+        
+        int mode = 0;
+        char[] codes = code.toCharArray();
+        for (int i=0; i<codes.length; i++) {
+            if (codes[i] == '1') {
+                mode = (mode == 0) ? 1 : 0;
+            }
+            
+            else {
+                // 짝수
+                if (i % 2 == 0) {
+                    if (mode == 0) ret.append(codes[i]);
+                }
+                
+                // 홀수
+                else {
+                    if (mode == 1) ret.append(codes[i]);
+                }
+            }
+        }
+        
+        String answer = (ret.length() == 0) ? "EMPTY" : ret.toString();
+        return answer;
+    }
+}
