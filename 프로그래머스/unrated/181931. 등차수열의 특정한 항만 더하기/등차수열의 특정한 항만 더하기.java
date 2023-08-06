69106b4d8527e39925c34ba2abccf26b66b20737
Changha, Choi <alklid@gmail.com>
2023-08-06 17:08:29 +0900

    [unrated] Title: 등차수열의 특정한 항만 더하기, Time: 2.94 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -1,3 +1,5 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int a, int d, boolean[] included) {
         /*
@@ -7,13 +9,18 @@ class Solution {
         }
         */
         
+        /*
         int answer = 0;
         for (int i=0; i<included.length; i++) {
             if (included[i]) {
                 answer += a + (i * d);
             }
         }
+        */
         
+        int answer = IntStream.range(0, included.length)
+                        .map(idx -> included[idx] ? a + (idx * d) : 0)
+                        .sum();
         return answer;
     }
 }
