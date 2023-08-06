f57bcca343fc20711fc70cba0c61c1f299393425
Changha, Choi <alklid@gmail.com>
2023-08-06 15:14:39 +0900

    [unrated] Title: 배열 만들기 6, Time: 23.64 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181859. 배열 만들기 6/README.md
+++ 프로그래머스/unrated/181859. 배열 만들기 6/README.md
@@ -1,10 +1,10 @@
 # [unrated] 배열 만들기 6 - 181859 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181859) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181859?language=java) 
 
 ### 성능 요약
 
-메모리: 83.7 MB, 시간: 14.68 ms
+메모리: 90.2 MB, 시간: 23.64 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.java
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.java
@@ -1,8 +1,10 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Stack;
 
 class Solution {
     public int[] solution(int[] arr) {
+        /*
         List<Integer> numbers = new ArrayList<>();
         
         for (int i=0; i<arr.length; i++) {
@@ -25,5 +27,29 @@ class Solution {
         
         int[] answer = numbers.stream().mapToInt(i -> i).toArray();
         return answer;
+        */
+        
+        Stack<Integer> numbers = new Stack<>();
+        for (int i=0; i<arr.length; i++) {
+            if (numbers.size() == 0) {
+                numbers.push(arr[i]);
+            }
+            else {
+                if (numbers.peek() == arr[i]) {
+                    numbers.pop();
+                }
+                else {
+                    numbers.push(arr[i]);
+                }
+            }
+        }
+        
+        if (numbers.size() == 0) {
+            return new int[]{-1};
+        }
+        
+        int[] answer = numbers.stream().mapToInt(i -> i).toArray();
+        return answer;
+        
     }
 }
