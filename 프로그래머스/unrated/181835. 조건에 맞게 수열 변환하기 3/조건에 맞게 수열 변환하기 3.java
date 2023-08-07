0bce916f730a563fa96b8ca8c60b9e407c88d2e6
Changha, Choi <alklid@gmail.com>
2023-08-07 23:46:00 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 6.07 ms, Memory: 114 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
@@ -0,0 +1,12 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        if (k % 2 == 0) {
+            return IntStream.of(arr).map(i -> i+k).toArray();
+        }
+        else {
+            return IntStream.of(arr).map(i -> i*k).toArray();
+        }
+    }
+}
