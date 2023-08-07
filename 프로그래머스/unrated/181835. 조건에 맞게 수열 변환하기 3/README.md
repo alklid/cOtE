da3bee3ebd61f7db9c5fdc6a0a1d2cd11ba60ca1
Changha, Choi <alklid@gmail.com>
2023-08-07 23:47:14 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 8.65 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/README.md
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/README.md
@@ -1,10 +1,10 @@
 # [unrated] 조건에 맞게 수열 변환하기 3 - 181835 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181835) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181835?language=java) 
 
 ### 성능 요약
 
-메모리: 114 MB, 시간: 6.07 ms
+메모리: 113 MB, 시간: 8.65 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
@@ -2,11 +2,14 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] arr, int k) {
+        /* 
         if (k % 2 == 0) {
             return IntStream.of(arr).map(i -> i+k).toArray();
         }
         else {
             return IntStream.of(arr).map(i -> i*k).toArray();
         }
+        */
+        return IntStream.of(arr).map(i -> k % 2 == 0 ? i+k : i*k).toArray();
     }
 }
