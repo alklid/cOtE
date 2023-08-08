0fb4a9a2ca42acdd3d480e3ddc64ac7c4764f9a7
Changha, Choi <alklid@gmail.com>
2023-08-08 19:44:57 +0900

    [level 0] Title: 배열 두배 만들기, Time: 19.97 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = Arrays.stream(numbers).map(i -> i * 2).toArray();
+        return answer;
+    }
+}
