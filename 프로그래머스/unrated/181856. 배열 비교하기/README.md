bb27a4e8c9e79b515e1a40cc08838f992b96e84f
Changha, Choi <alklid@gmail.com>
2023-08-08 16:26:13 +0900

    [unrated] Title: 배열 비교하기, Time: 0.04 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181856. 배열 비교하기/README.md
+++ 프로그래머스/unrated/181856. 배열 비교하기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 배열 비교하기 - 181856 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181856) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181856?language=java) 
 
 ### 성능 요약
 
-메모리: 82.6 MB, 시간: 0.03 ms
+메모리: 77.8 MB, 시간: 0.04 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.java
@@ -1,7 +1,9 @@
 import java.util.stream.IntStream;
+import java.lang.Integer;
 
 class Solution {
     public int solution(int[] arr1, int[] arr2) {
+        /*
         if (arr2.length == arr1.length) {
             int arr1Sum = IntStream.of(arr1).sum();
             int arr2Sum = IntStream.of(arr2).sum();
@@ -10,6 +12,14 @@ class Solution {
         }
 
         return arr1.length > arr2.length ? 1 : -1;
+        */
+        
+        int answer = Integer.compare(arr1.length, arr2.length);
+        if (answer == 0) {
+            answer = Integer.compare(IntStream.of(arr1).sum(), IntStream.of(arr2).sum());
+        }
+        
+        return answer;
         
     }
 }
