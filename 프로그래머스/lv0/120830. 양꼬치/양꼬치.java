b9c8b1f41a5d0d64e426839b4ccbcf9d054d0cdd
Changha, Choi <alklid@gmail.com>
2023-08-08 20:03:24 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int n, int k) {
-        int answer = (n * 12000) + ((k - n/10) * 2000);
+        int drink = ((k - n/10) < 0) ? 0 : (k - n/10);
+        int answer = (n * 12000) + (drink * 2000);
         return answer;
     }
 }
