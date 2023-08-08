75b6ab7d2a977bbf9c152b65f6e5a1f2aa34c988
Changha, Choi <alklid@gmail.com>
2023-08-08 16:34:47 +0900

    [unrated] Title: 문자열 정수의 합, Time: 1.27 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181849. 문자열 정수의 합/문자열 정수의 합.java
+++ 프로그래머스/unrated/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -1,8 +1,10 @@
 import java.util.stream.Stream;
+import java.lang.Character;
 
 class Solution {
     public int solution(String num_str) {
-        int answer = Stream.of(num_str.split("")).mapToInt(i -> Integer.parseInt(i)).sum();
+        // int answer = Stream.of(num_str.split("")).mapToInt(i -> Integer.parseInt(i)).sum();
+        int answer = num_str.chars().map(c -> Character.getNumericValue(c)).sum();
         return answer;
     }
 }
