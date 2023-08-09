d51044cc9e77f926057f747350789a8ebc787fe5
Changha, Choi <alklid@gmail.com>
2023-08-09 16:07:17 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,9 +1,13 @@
 class Solution {
     public int solution(int slice, int n) {
+        /*
         int answer = 1;
         while ((slice * answer) < n) {
             answer++;
         }
         return answer;
+        */
+        
+        return (n / slice) + ((n % slice > 0) ? 1 : 0);
     }
 }
