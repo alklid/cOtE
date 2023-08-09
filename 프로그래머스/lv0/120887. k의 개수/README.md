485cef6dbc003e22c70c43e7b061524e5cbddc07
Changha, Choi <alklid@gmail.com>
2023-08-09 20:56:15 +0900

    [level 0] Title: k의 개수, Time: 12.06 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/README.md
+++ 프로그래머스/lv0/120887. k의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 0] k의 개수 - 120887 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120887) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120887?language=java) 
 
 ### 성능 요약
 
-메모리: 85.5 MB, 시간: 11.63 ms
+메모리: 82.3 MB, 시간: 12.06 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -1,5 +1,6 @@
 class Solution {
     public int solution(int i, int j, int k) {
+        /*
         int answer = 0;
         String search = String.valueOf(k);
         for (int x=i; x<=j; x++) {
@@ -7,5 +8,13 @@ class Solution {
             answer += s.length() - s.replace(search, "").length();
         }
         return answer;
+        */
+        
+        StringBuffer sb = new StringBuffer();
+        for (int x=i; x<=j; x++) {
+            sb.append(String.valueOf(x));
+        }
+        
+        return sb.toString().length() - sb.toString().replace(String.valueOf(k), "").length();
     }
 }
