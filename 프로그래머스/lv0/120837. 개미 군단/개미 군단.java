732072fbb830aae7fbbd15ab424c427d77c49eca
Changha, Choi <alklid@gmail.com>
2023-08-09 17:00:49 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -1,14 +1,5 @@
 class Solution {
     public int solution(int hp) {
-        int answer = hp / 5;
-        hp -= (hp / 5) * 5;
-        
-        answer += hp / 3;
-        hp -= (hp / 3) * 3;
-        
-        answer += hp / 1;
-        hp -= (hp / 1) * 1;
-        
-        return answer;
+        return hp / 5 + (hp % 5 / 3) + hp % 5 % 3;
     }
 }
