72864333b29411eb8461dae94ba1f07bb634edcc
Changha, Choi <alklid@gmail.com>
2023-08-10 16:00:54 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 515.40 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/README.md
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 구슬을 나누는 경우의 수 - 120840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120840?language=java) 
 
 ### 성능 요약
 
-메모리: 77.6 MB, 시간: 0.73 ms
+메모리: 97.6 MB, 시간: 515.40 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
@@ -1,7 +1,21 @@
 import java.math.BigInteger;
 
+/**
+* nCr = n-1Cr-1 + n-1Cr
+*  n개중 r개를 뽑는 경우의 수는, 어떤 특정한 한 개를 포함하여 뽑는 경우와 특정한 한 개를 포함하지 않고 뽑는 경우의 수의 합과 같음.
+*  예) 5C3 = 4C2 + 4C3 
+*         = (3C1 + 3C2) + (3C2 + 3C3) 
+*         = ((2C0 + 2C1) + (2C1 + 2C2)) + ((2C1 + 2C2) + (2C2 + 2C3)) 
+*         = .... 계속 작은 수로 분리할 수 있음
+*
+*/
 class Solution {
     public int solution(int balls, int share) {
+        // nCr = n-1Cr-1 + n-1Cr 이용 방식
+        return division(balls,share);
+        
+        /*
+        // BigInterger 이용 방식
         BigInteger[] fact = new BigInteger[balls+1];
         factorial(fact);
         
@@ -10,9 +24,18 @@ class Solution {
         }
         
         return fact[balls].divide(fact[balls - share].multiply(fact[share])).intValue();
+        */
     }
-    
-    static void factorial(BigInteger[] fact) {
+
+    public int division(int balls, int share) {
+        if (balls == share || share == 0) {
+            return 1;
+        }
+        
+        return division(balls-1,share-1) + division(balls-1,share);
+    }        
+
+    public void factorial(BigInteger[] fact) {
         fact[0] = BigInteger.valueOf(1);
         fact[1] = BigInteger.valueOf(1);
         
