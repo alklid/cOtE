e0344217e1c85023bc495b66106db34d75b88414
Changha, Choi <alklid@gmail.com>
2023-08-10 18:36:51 +0900

    [level 0] Title: 문자열 밀기, Time: 1.17 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,3 +1,7 @@
+/**
+* 문자열을 오른쪽으로 한 칸씩 이동시키면서 특정 문자열과 같은지 확인
+* 확인만 하려면 repeat() 이용할 수 있으나 횟수를 구하려면 이동하면서 확인이 필요
+**/
 class Solution {
     public int solution(String A, String B) {
         int answer = 0;
