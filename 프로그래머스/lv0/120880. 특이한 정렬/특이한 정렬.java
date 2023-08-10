4197ef4bc2ccba13a56dab0fc20058baa3626ece
Changha, Choi <alklid@gmail.com>
2023-08-10 17:46:26 +0900

    [level 0] Title: 특이한 정렬, Time: 0.01 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
@@ -1,5 +1,18 @@
+/**
+* SelectionSort, 이중 loop 사용해서 정렬
+* 기준을 특정 수와 거리가 가까운 순서, 거리가 같아면 더 큰 수를 앞에 오도록 정렬
+**/
 class Solution {
     public int[] solution(int[] numlist, int n) {
+        /*
+        // Stream 이용 방식
+        return Arrays.stream(numList)
+                .boxed()
+                .sorted((a, b) -> Math.abs(a - n) == Math.abs(b - n) ? b.compareTo(a) : Integer.compare(Math.abs(a - n), Math.abs(b - n)))
+                .mapToInt(Integer::intValue)
+                .toArray();
+        */
+        
         for (int i=0; i<numlist.length-1; i++) {
             int minDiff = Math.abs(numlist[i]-n);
             
