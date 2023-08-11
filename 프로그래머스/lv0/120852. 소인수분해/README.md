9510e194f8e9e7d2cc7ff62630b10f6d78769a16
Changha, Choi <alklid@gmail.com>
2023-08-11 17:33:25 +0900

    [level 0] Title: 소인수분해, Time: 2.30 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120852. 소인수분해/README.md
+++ 프로그래머스/lv0/120852. 소인수분해/README.md
@@ -1,10 +1,10 @@
 # [level 0] 소인수분해 - 120852 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120852) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120852?language=java) 
 
 ### 성능 요약
 
-메모리: 77.3 MB, 시간: 4.45 ms
+메모리: 70.3 MB, 시간: 2.30 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -2,12 +2,30 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        List<Integer> primeFactorNumbers = factor(n);
-        return primeFactorNumbers.stream().mapToInt(i -> i).distinct().toArray();
+        // List<Integer> primeFactorNumbers = factor(n);
+        // return primeFactorNumbers.stream().mapToInt(i -> i).distinct().toArray();
+        
+        Set<Integer> primeFactorNumbers = factor2(n);
+        return primeFactorNumbers.stream().mapToInt(i -> i).toArray();
     }
     
     public List<Integer> factor(int num) {
         List<Integer> primeFactorNumbers = new ArrayList<>();
+
+        int sqrtNumber = ((Double) Math.sqrt(num)).intValue();
+        for (int div = 2; div <= sqrtNumber; div++) {
+            while (num % div == 0) {
+                num /= div;
+                primeFactorNumbers.add(div);
+            }
+        }
+
+        if (num > 1) primeFactorNumbers.add(num);
+        return primeFactorNumbers;
+    }
+    
+    public Set<Integer> factor2(int num) {
+        Set<Integer> primeFactorNumbers = new LinkedHashSet<>();
 
         int sqrtNumber = ((Double) Math.sqrt(num)).intValue();
         for (int div = 2; div <= sqrtNumber; div++) {
