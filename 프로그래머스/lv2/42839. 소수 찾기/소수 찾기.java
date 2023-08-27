f9a9ba34f02b357ea1070012d986fd0762036451
alklid <alklid@gmail.com>
2023-08-27 21:50:12 +0900

    순열과 조합 만들기

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -4,14 +4,20 @@ import java.util.Set;
 
 class Solution {
     public int solution(String numbers) {
-        // 순열 만들기
         int[] nums = Arrays.stream(numbers.split("")).mapToInt(Integer::parseInt).toArray();
         int[] result = new int[nums.length];
         boolean[] visited = new boolean[nums.length];
 
+        // 순열 만들기
         Set<Integer> perm = new HashSet<>();
         for (int i=0; i<nums.length; i++) {
             permutation(nums, result, visited, 0, i+1, perm);
+        }
+
+        // 조합 만들기
+        Set<Integer> comb = new HashSet<>();
+        for (int i=0; i<nums.length; i++) {
+            combination(nums, visited, 0, 0, i+1, comb);
         }
 
         // 경우의 수마다 isPrime 확인
@@ -25,10 +31,12 @@ class Solution {
     }
     
     // 순열 만들기
-    private void permutation(int[] nums, int[] result, boolean[] visited, int depth, int lv, Set<Integer> perm) {
-        if (depth == lv) {
+    // n 개의 값 중에서 r 개의 숫자를 순서를 고려해서 뽑는 경우
+    // [1, 2, 3]과 [1, 3, 2]는 다른 것으로 처리하는 경우
+    private void permutation(int[] nums, int[] result, boolean[] visited, int depth, int r, Set<Integer> perm) {
+        if (depth == r) {
             StringBuffer sb = new StringBuffer();
-            for (int i=0; i<lv; i++) {
+            for (int i=0; i<r; i++) {
                 sb.append(result[i]);
             }
             perm.add(Integer.parseInt(sb.toString()));
@@ -39,7 +47,29 @@ class Solution {
             if (!visited[i]) {
                 visited[i] = true;
                 result[depth] = nums[i];
-                permutation(nums, result, visited, depth + 1, lv, perm);
+                permutation(nums, result, visited, depth + 1, r, perm);
+                visited[i] = false;
+            }
+        }
+    }
+
+    // 조합 만들기
+    // n 개의 값 중에서 r 개의 숫자를 순서를 고려하지 않고 뽑는 경우
+    // [1, 2, 3]과 [1, 3, 2]는 같은 것으로 처리하는 경우
+    private void combination(int[] nums, boolean[] visited, int start, int depth, int r, Set<Integer> comb) {
+        if (depth == r) {
+            StringBuffer sb = new StringBuffer();
+            for (int i=0; i<nums.length; i++) {
+                if (visited[i]) sb.append(nums[i]);
+            }
+            comb.add(Integer.parseInt(sb.toString()));
+            return;
+        }
+
+        for (int i=0; i<nums.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                combination(nums, visited, i+1, depth + 1, r, comb);
                 visited[i] = false;
             }
         }
